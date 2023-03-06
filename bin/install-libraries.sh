667858b38282e36d8b93b5dd6f7413e1ed3e0fae
Martin Studer <martin@fluxlabs.ch>
2023-03-06 20:50:23 +0100

    Update install-libraries.sh

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -9,6 +9,6 @@ installLibrary() {
 }
 
 installLibrary flux-eco-ui-tree https://github.com/flux-eco/flux-eco-ui-tree/archive/refs/tags/v2023-02-23-1.tar.gz
-installLibrary flux-eco-ui-tree-state https://github.com/flux-eco/flux-eco-ui-tree-state/archive/refs/tags/v2023-02-23-1.tar.gz
+installLibrary flux-eco-ui-tree-state https://github.com/flux-eco/flux-eco-ui-tree-state/archive/refs/tags/v2023-03-07-1.tar.gz
 installLibrary flux-eco-ui-tree-element https://github.com/flux-eco/flux-eco-ui-tree-element/archive/refs/tags/v2023-02-23-1.tar.gz
 installLibrary flux-eco-ui-state-broadcaster https://github.com/flux-eco/flux-eco-ui-state-broadcaster/archive/refs/tags/v2023-02-23-1.tar.gz
